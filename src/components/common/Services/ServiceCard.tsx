9db938dce8aade68b4357a8eff7e04e539e59393
Subx1s0o <nazarsabardin@gmail.com>
2024-09-27 22:20:24 +0300

    fix: fix previous commit

--- src/components/common/Services/ServiceCard.tsx
+++ src/components/common/Services/ServiceCard.tsx
@@ -16,7 +16,7 @@ export default function ServiceCard({
 }: ServiceCardProps) {
   return (
     <li className="bg-white relative rounded-xl py-3 px-5 flex gap-10 justify-between items-center">
-      <h3 className="text-black inline-block  max-w-[230px] line-clamp-2">
+      <h3 className="text-black  max-w-[230px] line-clamp-2">
         #{index + 1} {service.title.uk}
       </h3>
       <p className="absolute left-[52%] -translate-x-1/2  text-black max-w-[500px] line-clamp-2">
