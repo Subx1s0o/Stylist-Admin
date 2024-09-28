b731d1669bdad0bf74ea594b5177a160b4a76205
Subx1s0o <nazarsabardin@gmail.com>
2024-09-28 05:30:01 +0300

    style: styling not found pages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,30 @@
+"use client";
+
+import Button from "@/components/ui/Button";
+import ServiceLayout from "@/views/ServiceLayout";
+import { useRouter } from "next/navigation";
+
 export default function NotFound() {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>This page does not exist. Please go back to the homepage.</p>
-    </div>
+    <ServiceLayout>
+      <div className="flex flex-col h-full justify-center items-center">
+        <h1 className=" text-xxl text-center mb-5">404</h1>
+        <p className=" text-md text-center mb-5 max-w-[400px]">
+          Ой! сторінку незнайдено, спробуйте будь ласка пізніше або перейдіть на
+          іншу сторінку.
+        </p>
+        <div className="">
+          <Button
+            variant="black"
+            className="mx-auto"
+            onClick={() => router.replace("/dashboard/services/style")}
+          >
+            Повернутися назад
+          </Button>
+        </div>
+      </div>
+    </ServiceLayout>
   );
 }
--- src/views/ServiceLayout.tsx
+++ src/views/ServiceLayout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function ServiceAddPage({ children }: { children: ReactNode }) {
   return (
-    <section className="bg-lightGrey rounded-[20px] p-[60px]">
+    <section className="bg-lightGrey rounded-[20px] p-[60px] h-full">
       {children}
     </section>
   );
